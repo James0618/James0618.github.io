8defae22061c680b4e62abad8f4718066a546f4a
James0618 <cjj_xjtu@163.com>
2018-10-05 14:47:21 +0800

    New

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,119 +1,41 @@
-/*
-	Prologue by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+var leftTab = document.getElementById('left-tab'),
+    rightTab = document.getElementById('right-tab');
 
-(function($) {
+// 垃圾方法，该方法必须等页面加载玩才有效
+// window.onload = function(){
+//   btn[0].onmouseover=function() {
+//     leftTab.style.display="block";
+//     leftTab.setAttribute("class","rotateInUpLeft self-animated");
+//   }
+//   btn[0].onmouseout=function() {
+//     leftTab.setAttribute("class","rotateOutDownLeft self-animated");
+//     // leftTab.style.display="none";
+//   }
+//   btn[1].onmouseover=function() {
+//     rightTab.style.display="block";
+//     rightTab.setAttribute("class","rotateInUpRight self-animated");
+//   }
+//   btn[1].onmouseout=function() {
+//     rightTab.setAttribute("class","rotateOutDownRight self-animated");
+//     // leftTab.style.display="none";
+//   }
+//
+// }
 
-	skel.breakpoints({
-		wide: '(min-width: 961px) and (max-width: 1880px)',
-		normal: '(min-width: 961px) and (max-width: 1620px)',
-		narrow: '(min-width: 961px) and (max-width: 1320px)',
-		narrower: '(max-width: 960px)',
-		mobile: '(max-width: 736px)'
-	});
+function showLeft() {
+  leftTab.style.display="block";
+  leftTab.setAttribute("class","rotateInUpLeft self-animated");
+}
 
-	$(function() {
+function goneLeft() {
+  leftTab.setAttribute("class","rotateOutDownLeft self-animated");
+}
 
-		var	$window = $(window),
-			$body = $('body');
+function showRight() {
+  rightTab.style.display="block";
+  rightTab.setAttribute("class","rotateInUpRight self-animated");
+}
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
-
-			$window.on('load', function() {
-				$body.removeClass('is-loading');
-			});
-
-		// CSS polyfills (IE<9).
-			if (skel.vars.IEVersion < 9)
-				$(':last-child').addClass('last-child');
-
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
-
-		// Prioritize "important" elements on mobile.
-			skel.on('+mobile -mobile', function() {
-				$.prioritize(
-					'.important\\28 mobile\\29',
-					skel.breakpoint('mobile').active
-				);
-			});
-
-		// Scrolly links.
-			$('.scrolly').scrolly();
-
-		// Nav.
-			var $nav_a = $('#nav a.scrolly');
-
-			// Scrolly-fy links.
-				$nav_a
-					.scrolly()
-					.on('click', function(e) {
-
-						var t = $(this),
-							href = t.attr('href');
-
-						if (href[0] != '#')
-							return;
-
-						e.preventDefault();
-
-						// Clear active and lock scrollzer until scrolling has stopped
-							$nav_a
-								.removeClass('active')
-								.addClass('scrollzer-locked');
-
-						// Set this link to active
-							t.addClass('active');
-
-					});
-
-			// Initialize scrollzer.
-				var ids = [];
-
-				$nav_a.each(function() {
-
-					var href = $(this).attr('href');
-
-					if (href[0] != '#')
-						return;
-
-					ids.push(href.substring(1));
-
-				});
-
-				$.scrollzer(ids, { pad: 200, lastHack: true });
-
-		// Header (narrower + mobile).
-
-			// Toggle.
-				$(
-					'<div id="headerToggle">' +
-						'<a href="#header" class="toggle"></a>' +
-					'</div>'
-				)
-					.appendTo($body);
-
-			// Header.
-				$('#header')
-					.panel({
-						delay: 500,
-						hideOnClick: true,
-						hideOnSwipe: true,
-						resetScroll: true,
-						resetForms: true,
-						side: 'left',
-						target: $body,
-						visibleClass: 'header-visible'
-					});
-
-			// Fix: Remove transitions on WP<10 (poor/buggy performance).
-				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-					$('#headerToggle, #header, #main')
-						.css('transition', 'none');
-
-	});
-
-})(jQuery);
+function goneRight() {
+  rightTab.setAttribute("class","rotateOutDownRight self-animated");
+}
